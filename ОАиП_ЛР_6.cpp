14322ac0a8912fb34675eddde7c346a17ffdde29
PetrachkovS <90954204+PetrachkovS@users.noreply.github.com>
2021-10-12 19:26:39 +0300

    Add files via upload

---
+++ ОАиП_ЛР_6.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	double** a, S;
+
+	int i, j, n;
+	S = 1;
+	cout << " Input size N : " << endl;
+	cin >> n;
+		a = new double* [n];
+		for (i = 0; i < n; i++)
+			a[i] = new double[n]; 
+		cout << "Input massive A : " << endl;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) 
+			cin>>a[i][j];
+		for (j = 0; j < n; j++)
+			for (i = 0; i < n; i++)
+			if (i+j < n-1) S *= a[i][j];
+	cout << "proizvedenie ravno :" << S << endl;
+	delete[]a;
+}
